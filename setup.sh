dc899ed7e8d07c3720698cfe4800c10e594825c1
Oli Larkin <olilarkin@googlemail.com>
2020-11-06 22:23:20 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ git submodule update
 cd iPlug2/Dependencies/IPlug/
 ./download-iplug-sdks.sh
 cd ../../..
-python duplicate.py MyNewPlugin $1 $2
+python3 duplicate.py MyNewPlugin $1 $2
 rm -r MyNewPlugin
 git add $1
 git commit -a -m "Renamed template project"
