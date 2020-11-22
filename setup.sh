1ce15ca33b7c8a41eceed6dcb5459a4e08e978a7
Oli Larkin <olilarkin@googlemail.com>
2020-11-22 12:12:13 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+# This script initializes the cloned iPlug2OOS repo, downloading dependencies and renaming the template
+
 if [ "$#" -eq 2 ]; then
+    echo "Initializing submodule..."
     git submodule init
     git submodule update
+    
+    echo "Downloading iPlug2 SDKs..."
     cd iPlug2/Dependencies/IPlug/
     ./download-iplug-sdks.sh
     cd ../../..
 
-    wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.2/mkcert-v1.4.2-linux-amd64 -o mkcert
-    ./mkcert install
-    ./mkcert localhost
-
+    echo "Duplicating template project..."
     python3 duplicate.py TemplateProject $1 $2
     rm -r TemplateProject
     git add $1
     git commit -a -m "Renamed template project"
+    
+    echo "Downloading mkcert..."
+    wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.2/mkcert-v1.4.2-linux-amd64 -o mkcert
+    ./mkcert install
+    ./mkcert localhost    
 else
     echo "usage: setup.sh PROJECT_NAME MANUFACTURER_NAME"
 fi
