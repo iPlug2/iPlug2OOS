dd548729ceade07604bb4e195c099d9c94f3efe3
Oli Larkin <olilarkin@googlemail.com>
2020-11-23 20:29:56 +0000

    get archive name separate

--- TemplateProject/scripts/makezip-win.py
+++ TemplateProject/scripts/makezip-win.py
@@ -5,14 +5,9 @@ projectpath = os.path.abspath(os.path.join(scriptpath, os.pardir))
 
 IPLUG2_ROOT = "../../iPlug2"
 
-sys.path.insert(0, os.path.join(scriptpath, IPLUG2_ROOT + '\Scripts'))
+sys.path.insert(0, os.path.join(os.getcwd(), IPLUG2_ROOT + '/Scripts'))
 
-from parse_config import parse_config
-
-MajorStr = ""
-MinorStr = "" 
-BugfixStr = ""
-BUNDLE_NAME = ""
+from get_archive_name import get_archive_name
 
 def main():
   if len(sys.argv) != 3:
@@ -21,9 +16,7 @@ def main():
   else:
     demo=int(sys.argv[1])
     zip=int(sys.argv[2])
- 
-  config = parse_config(projectpath)
-  
+
   dir = projectpath + "\\build-win\\out"
   
   if os.path.exists(dir):
@@ -51,12 +44,7 @@ def main():
       projectpath + "\\build-win\TemplateProject_x64.exe"  
     ]
 
-  zipname = "TemplateProject-v" + config['FULL_VER_STR']
-  
-  if demo:
-    zipname = zipname + "-win-demo"
-  else:
-    zipname = zipname + "-win"
+  zipname = get_archive_name(projectpath, "win", "demo" if demo == 1 else "full" )
 
   zf = zipfile.ZipFile(projectpath + "\\build-win\\out\\" + zipname + ".zip", mode="w")
 
@@ -79,10 +67,7 @@ def main():
     zf.write(f, os.path.basename(f), zipfile.ZIP_DEFLATED)
 
   zf.close()
-  print (zipname)
-  # print("wrote " + zipname)
-
-
+  print("wrote " + zipname)
 
 if __name__ == '__main__':
   main()
