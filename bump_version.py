1458385be17fdd31c107206f5e80c7c146f19784
Oli Larkin <olilarkin@googlemail.com>
2020-11-24 17:29:23 +0000

    Update bump_version.py

--- bump_version.py
+++ bump_version.py
@@ -28,22 +28,18 @@ def main():
   currentVersionInfo = semver.VersionInfo.parse(versionStr)
   print("current version in config.h: v" + versionStr)
 
-  newVersion = False
-
   if len(sys.argv) == 2:
     if sys.argv[1] == 'major':
       newVersionInfo = currentVersionInfo.bump_major()
-      newVersion = True
     elif sys.argv[1] == 'minor':
       newVersionInfo = currentVersionInfo.bump_minor()
-      newVersion = True
     elif sys.argv[1] == 'patch':
       newVersionInfo = currentVersionInfo.bump_patch()
-      newVersion = True
     else:
       newVersionInfo = currentVersionInfo
   else:
-    newVersionInfo = currentVersionInfo
+    print("Please supply an argument major, minor or patch")
+    exit()
 
   newVersionInt = (newVersionInfo.major << 16 & 0xFFFF0000) + (newVersionInfo.minor << 8 & 0x0000FF00) + (newVersionInfo.patch & 0x000000FF)
 
@@ -67,14 +63,12 @@ def main():
     os.system("cat " + PROJECT_ROOT + "/installer/changelog.txt")
     print("\n\n--------------------");
 
-  edit = input("\nTag version and push to origin? Y/N: ")
+  edit = input("\nTag version and git push to origin (will prompt for commit message)? Y/N: ")
 
   if edit == 'y' or edit == 'Y':
-    if newVersion:
-      os.system("git commit -a")
-      os.system("git tag v" + str(newVersionInfo))
+    os.system("git commit -a")
+    os.system("git tag v" + str(newVersionInfo))
     os.system("git push && git push --tags")
-    #os.system("git push origin refs/{\"v" + str(newVersionInfo) + "\",master}/*")
 
 if __name__ == '__main__':
   main()
