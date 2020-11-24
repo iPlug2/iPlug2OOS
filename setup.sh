4cf0334822274bf97a88d71b4047feb8bcca6636
Oli Larkin <olilarkin@googlemail.com>
2020-11-24 09:36:44 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,17 +13,17 @@ if [ "$#" -eq 2 ]; then
     cd ../../..
 
     echo "Duplicating template project..."
-    python3 duplicate.py TestOOS $1 $2
+    python3 duplicate.py TemplateProject $1 $2
     rm -r TestOOS
     git add $1
     git commit -a -m "Renamed template project"
     
-    echo "Downloading mkcert..."
-    wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.2/mkcert-v1.4.2-linux-amd64
-    chmod +x mkcert-v1.4.2-linux-amd64
-    ./mkcert-v1.4.2-linux-amd64 install
-    ./mkcert-v1.4.2-linux-amd64 localhost
-    rm mkcert-v1.4.2-linux-amd64
+#     echo "Downloading mkcert..."
+#     wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.2/mkcert-v1.4.2-linux-amd64
+#     chmod +x mkcert-v1.4.2-linux-amd64
+#     ./mkcert-v1.4.2-linux-amd64 install
+#     ./mkcert-v1.4.2-linux-amd64 localhost
+#     rm mkcert-v1.4.2-linux-amd64
 else
     echo "usage: setup.sh PROJECT_NAME MANUFACTURER_NAME"
 fi
