6475de0573550ac7f7c7fd7d4c58a9a7d821cbf2
Oli Larkin <olilarkin@googlemail.com>
2021-01-09 11:35:44 +0000

    don't duplicate

--- setup_container.sh
+++ setup_container.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-# This script initializes the cloned iPlug2OOS repo, downloading dependencies, 
-# renaming the template and generating an SSL certificate using mkcert
+# This script initializes the cloned iPlug2OOS repo, downloading dependencies, and tools
 
-if [ "$#" -eq 2 ]; then
-    echo "Initializing submodule..."
-    git submodule update --init
-    
-    echo "Downloading iPlug2 SDKs..."
-    cd iPlug2/Dependencies/IPlug/
-    ./download-iplug-sdks.sh
-    cd ../../..
+echo "Initializing submodule..."
+git submodule update --init
 
-    echo "Duplicating template project..."
-    python3 duplicate.py TemplateProject $1 $2
-    rm -r TemplateProject
-    git add $1
-    git commit -a -m "Renamed template project"
-    
-    echo "Downloading mkcert..."
-    wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64 -O mkcert
-    chmod +x mkcert
-    mv mkcert /usr/local/bin
-    mkcert -install
-else
-    echo "usage: setup.sh PROJECT_NAME MANUFACTURER_NAME"
-fi
+echo "Downloading iPlug2 SDKs..."
+cd iPlug2/Dependencies/IPlug/
+./download-iplug-sdks.sh
+cd ../../..
+
+echo "Downloading mkcert..."
+wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64 -O mkcert
+chmod +x mkcert
+mv mkcert /usr/local/bin
+mkcert -install
+
+echo "Downloading SOUL"
+wget https://github.com/soul-lang/SOUL/releases/download/0.9.66/binaries-linux-combined.zip
+unzip binaries-linux-combined.zip
+mv linux/x64/soul /usr/local/bin
+rm -r linux
+rm binaries-linux-combined.zip
