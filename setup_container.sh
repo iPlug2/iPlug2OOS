d7c7b7c68cadb3ca5a7f1f86c541b79ab1946e07
Oli Larkin <olilarkin@googlemail.com>
2021-01-09 11:55:48 +0000

    don't dl soul for now

--- setup_container.sh
+++ setup_container.sh
@@ -10,15 +10,20 @@ cd iPlug2/Dependencies/IPlug/
 ./download-iplug-sdks.sh
 cd ../../..
 
+echo "Downloading iPlug2 prebuilt libs..."
+cd iPlug2/Dependencies/
+./download-prebuilt-libs.sh
+cd ../..
+
 echo "Downloading mkcert..."
 wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64 -O mkcert
 chmod +x mkcert
 mv mkcert /usr/local/bin
 mkcert -install
 
-echo "Downloading SOUL"
-wget https://github.com/soul-lang/SOUL/releases/download/0.9.66/binaries-linux-combined.zip
-unzip binaries-linux-combined.zip
-mv linux/x64/soul /usr/local/bin
-rm -r linux
-rm binaries-linux-combined.zip
+# echo "Downloading SOUL"
+# wget https://github.com/soul-lang/SOUL/releases/download/0.9.66/binaries-linux-combined.zip
+# unzip binaries-linux-combined.zip
+# mv linux/x64/soul /usr/local/bin
+# rm -r linux
+# rm binaries-linux-combined.zip
